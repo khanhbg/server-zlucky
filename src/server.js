aa30a3fa03efbdbfb33022b40f802edd9fee025c
khanhbg <105843389+khanhbg@users.noreply.github.com>
2023-01-12 15:36:36 +0700

    deployfirt

--- src/server.js
+++ src/server.js
@@ -1,21 +1,21 @@
 import express from "express";
-import bodyParser from 'body-parser';
-import initWebRoutes from './routes/web.js';
-import cookieParser from 'cookie-parser';
-import connectD from "./config/connectDB.js";
-import dotenv from 'dotenv';
-const Nexmo = require('nexmo');
+//import bodyParser from 'body-parser';
+//import initWebRoutes from './routes/web.js';
+//import cookieParser from 'cookie-parser';
+//import connectD from "./config/connectDB.js";
+//import dotenv from 'dotenv';
+//const Nexmo = require('nexmo');
 let app= express()
-app.use(cookieParser())
+//app.use(cookieParser())
 
-app.use(function (req, res, next) {
-    res.header("Access-Control-Allow-Origin", "http://localhost:4200");
-    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
-    res.header("Access-Control-Allow-Credentials", "true")
-    next();
-  })
-app.use(bodyParser.urlencoded({ extended: true }));
-app.use(bodyParser.json());
+// app.use(function (req, res, next) {
+//     res.header("Access-Control-Allow-Origin", "http://localhost:4200");
+//     res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+//     res.header("Access-Control-Allow-Credentials", "true")
+//     next();
+//   })
+// app.use(bodyParser.urlencoded({ extended: true }));
+// app.use(bodyParser.json());
 
 
 // const nexmo = new Nexmo({
