f03cb8933055baac79565230b0aa451969d94f42
khanhbg <105843389+khanhbg@users.noreply.github.com>
2023-01-12 15:38:56 +0700

    deployfirt

--- src/server.js
+++ src/server.js
@@ -7,7 +7,6 @@ import express from "express";
 //const Nexmo = require('nexmo');
 let app= express()
 //app.use(cookieParser())
-
 // app.use(function (req, res, next) {
 //     res.header("Access-Control-Allow-Origin", "http://localhost:4200");
 //     res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
