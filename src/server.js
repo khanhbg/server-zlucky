ec8c880f620d4dc3122b07ab00406e75b8fdac01
khanhbg <105843389+khanhbg@users.noreply.github.com>
2023-01-14 14:02:22 +0700

    first commit

--- src/server.js
+++ src/server.js
@@ -8,7 +8,7 @@ import dotenv from 'dotenv';
 let app= express()
 app.use(cookieParser())
 app.use(function (req, res, next) {
-    res.header("Access-Control-Allow-Origin", "http://localhost:4200"); 
+    res.header("Access-Control-Allow-Origin", "http://localhost:4200/"); 
     res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
     res.header("Access-Control-Allow-Credentials", "true")
     next();
