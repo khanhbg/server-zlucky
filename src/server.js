997b6b1d7bb9750dcac93d88e599482dbcc4f468
khanhbg <105843389+khanhbg@users.noreply.github.com>
2023-01-18 08:45:36 +0700

    first commit

--- src/server.js
+++ src/server.js
@@ -9,7 +9,7 @@ let app= express()
 app.use(cookieParser())
 
 app.use(function (req, res, next) { 
-    res.header("Access-Control-Allow-Origin", "https://zolucky.onrender.com"); 
+    res.header("Access-Control-Allow-Origin", "https://czolucky.vercel.app"); 
     res.header("Access-Control-Allow-Methods", "GET,HEAD,OPTIONS,POST,PUT");
     res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
     res.header("Access-Control-Allow-Credentials", "true");
